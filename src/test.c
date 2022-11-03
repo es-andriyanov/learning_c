ed80c7bf389a8b4fcb95988fb258db1a62710742
es-andriyanov <es.andriyanov@gmail.com>
2022-11-03 16:45:05 +0700

    develop: gitlab connected

--- src/test.c
+++ src/test.c
@@ -4,6 +4,7 @@ int main() {
     printf("Hello from WSL2 :)\n");
     printf("Testing Github and Gitlab connection\n");
     printf("Another testing Github and Gitlab connection\n");
+    printf("Another one testing Github and Gitlab connection\n");
 
     return 0;
 }
