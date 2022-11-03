d28aa872f26755233a0eedf45dc32c9421c6d71d
Adina Maritza <adinamar@student.21-school.ru>
2022-11-03 16:42:00 +0700

    develop: connected gitlab

--- src/test.c
+++ src/test.c
@@ -3,6 +3,7 @@
 int main() {
     printf("Hello from WSL2 :)\n");
     printf("Testing Github and Gitlab connection\n");
+    printf("Another testing Github and Gitlab connection\n");
 
     return 0;
 }
